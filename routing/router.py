b634d3bfdd7939aae462f4b8d83eeb10e5e70578
Roman <roma_klimovitskiy@mail.ru>
2021-05-06 23:00:19 +0300

    Scatter implementation via flooding through ideal naive proxies

--- routing/router.py
+++ routing/router.py
@@ -17,6 +17,7 @@ class Router:
     __KEEPALIVE_TIMEOUT = __SCATTER_RT_TIMEOUT
 
     ROUTER_SERVER_PORT = 4200
+    ROUTE_UPDATES_TOPIC = 'router'
 
     def __init__(self, config: Config, metric_service: MetricService, scatterer: Scatterer):
         self.__id = config.get_local_node_id()
@@ -26,11 +27,8 @@ class Router:
         self.__metric_service = metric_service
         self.__scatterer = scatterer
 
-        self.__rt_version = 0
-        self.__last_seen_rt_versions: tp.Dict[int, tp.Dict[int, int]] = {}
-
         self.__route_table = routing_pb2.RouteTable()
-        self.__route_table.routes[self.__group] = routing_pb2.Route(next_hop=-1, metric=0)
+        self.__route_table.routes[self.__group] = routing_pb2.Route(next_hop=-1, metric=0, length=0)
         for group_id in self.__groups:
             self.__set_naive_route(group_id)
 
@@ -54,7 +52,9 @@ class Router:
             direct_metric = self.__metric_service.get_direct_metric(node)
             if best_direct_metric is None or best_direct_metric > direct_metric:
                 best_direct_metric = direct_metric
-                self.__route_table.routes[target_group_id] = routing_pb2.Route(next_hop=node, metric=direct_metric)
+                self.__route_table.routes[target_group_id] = routing_pb2.Route(
+                    next_hop=node, metric=direct_metric, length=1
+                )
 
     async def __run_server(self) -> None:
         loop = asyncio.get_event_loop()
@@ -63,13 +63,11 @@ class Router:
             local_addr=('0.0.0.0', self.ROUTER_SERVER_PORT))
 
     async def __scatter_updates(self, updated_routes: tp.Dict[int, routing_pb2.Route]) -> None:
-        emergency_update = routing_pb2.UpdateMessage(
-            rt_version=self.__rt_version,
+        update_message = routing_pb2.RouteUpdateMessage(
             source_node=self.__id,
             updated_routes=updated_routes,
         )
-        self.__rt_version += 1
-        await self.__scatterer.scatter(emergency_update.SerializeToString())
+        await self.__scatterer.scatter(update_message.SerializeToString(), self.ROUTE_UPDATES_TOPIC)
 
     async def __availability_checker_callback(self) -> None:
         reset_routes: tp.Dict[int, routing_pb2.Route] = {}
@@ -84,6 +82,10 @@ class Router:
     def get_server_port() -> int:
         return Router.ROUTER_SERVER_PORT
 
+    @staticmethod
+    def get_topic() -> str:
+        return Router.ROUTE_UPDATES_TOPIC
+
     def stop(self) -> None:
         self.__scatter_rt_timer.cancel()
         if self.__transport is not None:
@@ -91,36 +93,37 @@ class Router:
         else:
             self.__run_server_task.cancel()
 
+    def get_route(self, target_group: int) -> routing_pb2.Route:
+        return self.__route_table.routes[target_group]
+
     def get_next_hop(self, target_group: int) -> tp.Optional[int]:
         if self.__group == target_group:
             return None
         else:
             return self.__route_table.routes[target_group].next_hop
 
-    async def handle_update(self, update_message: routing_pb2.UpdateMessage) -> None:
+    async def handle_update(self, update_message: routing_pb2.RouteUpdateMessage) -> None:
         emergency_updates: tp.Dict[int, routing_pb2.Route] = {}
         for group, route in update_message.updated_routes.items():
-            self.__update_route(group, update_message.source_node, route, update_message.rt_version, emergency_updates)
+            self.__update_route(group, update_message.source_node, route, emergency_updates)
 
         await self.__scatter_updates(emergency_updates)
 
     def __update_route(self, target_group: int, proposed_next_hop: int,
-                       proposed_route: routing_pb2.Route, rt_version: int,
+                       proposed_route: routing_pb2.Route,
                        emergency_updates: tp.Dict[int, routing_pb2.Route]) -> None:
-        if proposed_next_hop in self.__last_seen_rt_versions[target_group] and \
-                self.__last_seen_rt_versions[target_group][proposed_next_hop] > rt_version:
-            return
-        else:
-            self.__last_seen_rt_versions[target_group][proposed_next_hop] = rt_version
-
         current_route = self.__route_table.routes[target_group]
         proposed_metric = proposed_route.metric + self.__metric_service.get_direct_metric(proposed_next_hop)
+        proposed_length = proposed_route.length + 1
         old_metric = current_route.metric
-        if proposed_metric < current_route.metric:
+        if proposed_metric < current_route.metric or \
+                (proposed_metric == current_route.metric and proposed_length < current_route.length):
             current_route.next_hop = proposed_next_hop
             current_route.metric = proposed_metric
+            current_route.length = proposed_length
         elif current_route.next_hop == proposed_next_hop:
             current_route.metric = proposed_metric
+            current_route.length = proposed_length
 
         if abs(old_metric - current_route.metric) >= self.__get_emergency_metric_delta():
             emergency_updates[target_group] = current_route
@@ -135,7 +138,7 @@ class RouterHandler(DataHandler):
         self.__router = router
 
     async def handle(self, data: bytes) -> None:
-        update_message = routing_pb2.UpdateMessage()
+        update_message = routing_pb2.RouteUpdateMessage()
         try:
             update_message.ParseFromString(data)
             await self.__router.handle_update(update_message)
--- routing/scatter.py
+++ routing/scatter.py
@@ -15,6 +15,8 @@ class Scatterer:
     def __init__(self, config: Config) -> None:
         self.__config = config
 
+        self.__id = config.get_local_node_id()
+
         groups_to_nodes_dict = config.get_groups_to_nodes_dict()
         self.__group_id = config.get_local_group_id()
         self.__group = groups_to_nodes_dict[self.__group_id]
@@ -24,22 +26,15 @@ class Scatterer:
 
         self.__router: tp.Optional[Router] = None
 
+        self.__topic_versions: tp.Dict[str, int] = {}
+        self.__last_seen_topic_versions: tp.Dict[str, tp.Dict[int, int]] = {}
+
         self.__transport: tp.Optional[asyncio.BaseTransport] = None
         self.__run_server_task = asyncio.create_task(self.__run_server())
 
     @staticmethod
     def __calc_ttl_init_value(groups_to_nodes_dict: tp.Dict[int, tp.Set[int]]) -> int:
-        ttl_init_value = 0
-        smallest_group_size: tp.Optional[int] = None
-        for group_set in groups_to_nodes_dict.items():
-            ttl_init_value += len(group_set)
-            if smallest_group_size is None or len(group_set) < smallest_group_size:
-                smallest_group_size = len(group_set)
-
-        if ttl_init_value > 0 and smallest_group_size is not None:
-            ttl_init_value -= smallest_group_size + 1
-
-        return ttl_init_value
+        return len(groups_to_nodes_dict) * 2 - 1
 
     async def __run_server(self) -> None:
         loop = asyncio.get_event_loop()
@@ -56,52 +51,54 @@ class Scatterer:
         else:
             self.__run_server_task.cancel()
 
-    async def scatter(self, data: bytes) -> None:
+    async def scatter(self, data: bytes, topic: str) -> None:
+        topic_version = 0
+        if topic in self.__topic_versions:
+            topic_version = self.__topic_versions[topic]
+
+        self.__topic_versions[topic] = topic_version + 1
         scatter_message = routing_pb2.ScatterMessage(
-            dest_groups=[],
             data=data,
             ttl=self.__ttl_init_value,
+            topic=topic,
+            source_node=self.__id,
+            topic_version=topic_version,
+            last_group=-1,
         )
-        for group in self.__groups:
-            scatter_message.dest_groups.append(group)
 
         await self.handle_scatter_message(scatter_message)
 
     async def handle_scatter_message(self, message: routing_pb2.ScatterMessage) -> None:
         assert self.__router is not None
-        next_hops: tp.Dict[int, routing_pb2.ScatterMessage] = {}
 
-        message.ttl -= 1
-        if message.ttl <= 0:
-            await self.__handle_expired_message(message)
+        if message.topic in self.__last_seen_topic_versions and \
+                message.source_node in self.__last_seen_topic_versions[message.topic] and \
+                message.topic_version <= self.__last_seen_topic_versions[message.topic][message.source_node]:
             return
 
-        for dest_group in message.dest_groups:
-            if dest_group == self.__group_id:
-                await self.__scatter_within_group(message.data)
-            else:
-                next_hop = self.__router.get_next_hop(dest_group)
-                assert next_hop is not None
-                if next_hop in next_hops:
-                    next_hops[next_hop].dest_groups.append(dest_group)
-                else:
-                    next_hops[next_hop] = routing_pb2.ScatterMessage(
-                        dest_groups=[dest_group],
-                        data=message.data,
-                        ttl=message.ttl
-                    )
-
-        for next_hop, message in next_hops.items():
-            await network.send_data(
-                self.__config.get_ip_addr_by_node_id(next_hop),
-                self.SCATTER_SERVER_PORT,
-                message.SerializeToString()
-            )
-
-    async def __handle_expired_message(self, message: routing_pb2.ScatterMessage) -> None:
-        assert message.ttl <= 0
-        if self.__group_id in message.dest_groups:
+        self.__last_seen_topic_versions[message.topic][message.source_node] = message.topic_version
+        last_group = message.last_group
+        message.ttl -= 1
+        if message.ttl <= 0 and last_group != self.__group_id:
             await self.__scatter_within_group(message.data)
+            return
+
+        message.last_group = self.__group_id
+        for group in self.__groups:
+            if group == last_group:
+                continue
+
+            if group == self.__group_id:
+                await self.__scatter_within_group(message.data)
+                continue
+
+            route_to_group = self.__router.get_route(group)
+            if route_to_group.length == 1:
+                await network.send_data(
+                    self.__config.get_ip_addr_by_node_id(route_to_group.next_hop),
+                    self.SCATTER_SERVER_PORT,
+                    message.SerializeToString()
+                )
 
     async def __scatter_within_group(self, data: bytes) -> None:
         for node in self.__group:
