dc13f9360153035f759a525a80bc1578717b47b6
Roman <roma_klimovitskiy@mail.ru>
2021-05-20 18:28:53 +0300

    Added state expiration logic

--- routing/reducer_designator.py
+++ routing/reducer_designator.py
@@ -18,6 +18,7 @@ class NeighbourRevision:
 class ReducerDesignator:
     __SEND_HEARTBEAT_TIMEOUT = 10
     __DEAD_TIMEOUT = __SEND_HEARTBEAT_TIMEOUT * 3
+    __STATE_EXPIRATION = 1
 
     HEARTBEAT_TOPIC = 'heartbeat'
 
@@ -27,6 +28,8 @@ class ReducerDesignator:
         self.__scatterer = scatterer
         self.__reducer = NeighbourRevision(-1, -1)
         self.__backup_reducer = NeighbourRevision(-1, -1)
+        self.__reducer_till_expiration = -1
+        self.__backup_till_expiration = -1
         self.__received_heartbeats: tp.Dict[int, routing_pb2.HeartbeatMessage] = {}
 
         self.__heartbeat_timer = Repeater(self.__SEND_HEARTBEAT_TIMEOUT, self.__send_heartbeat, True)
@@ -38,21 +41,24 @@ class ReducerDesignator:
         if heartbeat.state == REDUCER:
             if self.__reducer.id < source_revision.id or \
                     self.__is_new_revision(self.__reducer, source_revision):
-                self.__reducer = source_revision
+                self.__set_reducer(source_revision)
                 if self.__reducer.id == self.__backup_reducer.id:
-                    self.__backup_reducer = NeighbourRevision(-1, -1)
+                    self.__set_backup(NeighbourRevision(-1, -1))
+            elif self.__is_same(self.__reducer, source_revision):
+                self.__reducer_till_expiration = self.__STATE_EXPIRATION
         elif heartbeat.state == BACKUP_REDUCER:
             if self.__is_same(self.__reducer, source_revision):
                 return
-
             if self.__backup_reducer.id < source_revision.id or \
                     self.__is_new_revision(self.__backup_reducer, source_revision):
-                self.__backup_reducer = source_revision
+                self.__set_backup(source_revision)
+            elif self.__is_same(self.__backup_reducer, source_revision):
+                self.__backup_till_expiration = self.__STATE_EXPIRATION
         elif heartbeat.state == OTHER:
             if self.__is_new_revision(self.__reducer, source_revision):
-                self.__reducer = NeighbourRevision(-1, -1)
+                self.__set_reducer(NeighbourRevision(-1, -1))
             elif self.__is_new_revision(self.__backup_reducer, source_revision):
-                self.__backup_reducer = NeighbourRevision(-1, -1)
+                self.__set_backup(NeighbourRevision(-1, -1))
 
     @staticmethod
     def __is_new_revision(prev: NeighbourRevision, cur: NeighbourRevision) -> bool:
@@ -72,21 +78,36 @@ class ReducerDesignator:
         await self.__scatterer.scatter_local(heartbeat.SerializeToString(), self.HEARTBEAT_TOPIC)
 
     def __check_availability(self) -> None:
-        if not self.__is_node_alive(self.__reducer, REDUCER) or \
-                not self.__is_node_alive(self.__backup_reducer, BACKUP_REDUCER):
+        if not self.__is_node_alive(self.__reducer) or not self.__is_node_alive(self.__backup_reducer):
             self.__reelect()
+        else:
+            if not self.__is_in_state(self.__reducer.id, REDUCER):
+                self.__reducer_till_expiration -= 1
+            if not self.__is_in_state(self.__backup_reducer.id, BACKUP_REDUCER):
+                self.__backup_till_expiration -= 1
+            if self.__reducer_till_expiration < 0 or self.__backup_till_expiration < 0:
+                self.__reelect()
 
         self.__received_heartbeats.clear()
 
+    def __is_node_alive(self, node: NeighbourRevision) -> bool:
+        return node.id in self.__received_heartbeats and \
+               node.start_ts == self.__received_heartbeats[node.id].start_ts
+
+    def __is_in_state(self, node_id: int, required_state: routing_pb2.NeighbourState.V) -> bool:
+        if node_id not in self.__received_heartbeats:
+            return False
+        return self.__received_heartbeats[node_id].state == required_state
+
     def __reelect(self) -> None:
-        if not self.__is_node_alive(self.__reducer, REDUCER):
-            self.__reducer = self.__backup_reducer
-            self.__backup_reducer = NeighbourRevision(-1, -1)
+        if not self.__is_node_alive(self.__reducer) or self.__reducer_till_expiration < 0:
+            self.__set_reducer(self.__backup_reducer)
+            self.__set_backup(NeighbourRevision(-1, -1))
 
         available_nodes = self.__received_heartbeats.keys()
         if len(available_nodes) <= 1:
-            self.__reducer = NeighbourRevision(self.__id, self.__start_ts)
-            self.__backup_reducer = NeighbourRevision(-1, -1)
+            self.__set_reducer(NeighbourRevision(self.__id, self.__start_ts))
+            self.__set_backup(NeighbourRevision(-1, -1))
             return
 
         possible_backups = set(filter(
@@ -102,7 +123,7 @@ class ReducerDesignator:
             possible_backups
         ))
 
-        self.__backup_reducer = self.__elect_from(possible_backups, self_proclaimed_backups)
+        self.__set_backup(self.__elect_from(possible_backups, self_proclaimed_backups))
         if self.__is_node_alive(self.__reducer):
             return
 
@@ -116,13 +137,21 @@ class ReducerDesignator:
             possible_reducers,
         ))
 
-        self.__reducer = self.__elect_from(possible_reducers, self_proclaimed_reducers)
+        self.__set_reducer(self.__elect_from(possible_reducers, self_proclaimed_reducers))
 
-    def __is_node_alive(self, node: NeighbourRevision,
-                        required_state: tp.Optional[routing_pb2.NeighbourState.V] = None) -> bool:
-        return node.id in self.__received_heartbeats and \
-               node.start_ts == self.__received_heartbeats[node.id].start_ts and \
-               (required_state is None or self.__received_heartbeats[node.id].state == required_state)
+    def __set_reducer(self, node: NeighbourRevision) -> None:
+        self.__reducer = node
+        if node.id >= 0:
+            self.__reducer_till_expiration = self.__STATE_EXPIRATION
+        else:
+            self.__reducer_till_expiration = -1
+
+    def __set_backup(self, node: NeighbourRevision) -> None:
+        self.__backup_reducer = node
+        if node.id >= 0:
+            self.__backup_till_expiration = self.__STATE_EXPIRATION
+        else:
+            self.__backup_till_expiration = -1
 
     def __elect_from(self, possible: tp.Set[int], self_proclaimed: tp.Set[int]) -> NeighbourRevision:
         new_elect = -1
